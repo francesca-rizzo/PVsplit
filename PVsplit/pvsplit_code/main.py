5590f5152473f1a6e8762f23bee9a3530607f874
francesca-rizzo <79145299+francesca-rizzo@users.noreply.github.com>
2024-05-03 11:18:19 +0200

    Update example file in main.py

--- PVsplit/pvsplit_code/main.py
+++ PVsplit/pvsplit_code/main.py
@@ -4,7 +4,7 @@ import outputs
 
 def main():
     input_folder = "../example_directory/output" # Path to the folder containing the “pvs” and “plotscripts” directories
-    gname = "serra_00_snap_23_h1842_i_1.38_ca_2.9_4_2_3.alma_0.05arcsec_0" # Name of the output files in the pvs folder
+    gname = "serra_02_snap_64_h3043_i_1.05_2.3_1.3.alma_0.1arcsec_0" # Name of the output files in the pvs folder
     outfolder = read_pvs_plot.find_outfolder(input_folder)
     paths = []
     p = read_pvs_plot.fetch_pvs_plit(outfolder, paths)
